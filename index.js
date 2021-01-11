0abd1361c4d2105f5dc50bb945b2cae7bd9fd63a
thakur00 <arunbhadouriya97@gmail.com>
2021-01-11 14:03:15 +0530

    added more of linkedlist data
    Signed-off-by: thakur00 <arunbhadouriya97@gmail.com>

---
+++ index.js
@@ -0,0 +1,28 @@
+const question = document.getElementsByClassName('question');
+const pre =document.getElementsByTagName('pre');
+
+function getIndex(arr){
+    const len=arr.length;
+    for(let i=0;i<len;i++){
+        if(arr[i].tagName=="PRE") return i;
+    }
+    return -1;
+}
+
+document.addEventListener('click', event =>{
+    console.log(Array.from(event.target.childNodes))
+    const arr=Array.from(event.target.childNodes);
+    const indexOfpreElement = getIndex(arr)
+
+
+    
+    if(event.target.classList.value=="question"){
+
+        if(event.target.childNodes[indexOfpreElement].style.display == "none"){
+            event.target.childNodes[indexOfpreElement].style.display="block";
+        }else{
+            event.target.childNodes[indexOfpreElement].style.display="none";
+        }
+    }
+
+});
