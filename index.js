257b7d92ee60f092a062795d8fb38740ab952f5b
thakur00 <arunbhadouriya97@gmail.com>
2021-01-12 13:56:51 +0530

    added styles and changed the html structure
    Signed-off-by: thakur00 <arunbhadouriya97@gmail.com>

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const question = document.getElementsByClassName('question');
-const pre =document.getElementsByTagName('pre');
+// const question = document.getElementsByClassName('question');
+// const pre =document.getElementsByTagName('pre');
 
-function getIndex(arr){
-    const len=arr.length;
-    for(let i=0;i<len;i++){
-        if(arr[i].tagName=="PRE") return i;
-    }
-    return -1;
-}
+// function getIndex(arr){
+//     const len=arr.length;
+//     for(let i=0;i<len;i++){
+//         if(arr[i].tagName=="PRE") return i;
+//     }
+//     return -1;
+// }
 
-document.addEventListener('click', event =>{
-    console.log(Array.from(event.target.childNodes))
-    const arr=Array.from(event.target.childNodes);
-    const indexOfpreElement = getIndex(arr)
+// document.addEventListener('click', event =>{
+//     console.log(Array.from(event.target.childNodes))
+//     const arr=Array.from(event.target.childNodes);
+//     const indexOfpreElement = getIndex(arr)
 
 
     
-    if(event.target.classList.value=="question"){
+//     if(event.target.classList.value=="question"){
 
-        if(event.target.childNodes[indexOfpreElement].style.display == "none"){
-            event.target.childNodes[indexOfpreElement].style.display="block";
-        }else{
-            event.target.childNodes[indexOfpreElement].style.display="none";
-        }
-    }
+//         if(event.target.childNodes[indexOfpreElement].style.display == "none"){
+//             event.target.childNodes[indexOfpreElement].style.display="block";
+//         }else{
+//             event.target.childNodes[indexOfpreElement].style.display="none";
+//         }
+//     }
 
-});
+// });
